365e4296aa48621020476056fca3d6ff72d1049b
RobertHJones <roberthuwjones@outlook.com>
2022-04-11 13:13:18 +0100

    dropdown sorted

--- Components/Input/index.js
+++ Components/Input/index.js
@@ -6,29 +6,45 @@ import DropDownPicker from "react-native-dropdown-picker";
 export default function Input({ onSubmit }) {
   const [text, onChangeText] = useState("");
   const [open, setOpen] = useState(false);
-  const [strongValue, setStrongValue] = useState(null);
+  const [value, setValue] = useState(null);
   const [stretchValue, setStretchValue] = useState(null);
   const [seriesValue, setSeriesValue] = useState(null);
-  const [strongItems, setStrongItems] = useState([
-    { label: "Back", value: "Back" },
-    { label: "Abdomen", value: "Abdomen" },
-    { label: "Thigh", value: "Thigh" },
-    { label: "Hip", value: "Hip" },
-    { label: "Knee", value: "Knee" },
+  const [items, setItems] = useState([
+    { label: "Area Stretched", value: "Area Stretched" },
+    { label: "Area Strengthend", value: "Area Strengthend" },
+    { label: "Series", value: "Series" },
+    { label: "Back", value: "Back", parent: "Area Strengthend" },
+    { label: "Abdomen", value: "Abdomen", parent: "Area Strengthend" },
+    { label: "Thigh", value: "Thigh", parent: "Area Strengthend" },
+    { label: "Hip", value: "Hip", parent: "Area Strengthend" },
+    { label: "Knee", value: "Knee", parent: "Area Strengthend" },
+    { label: "Back", value: "Backstretch", parent: "Area Stretched" },
+    { label: "Abdomen", value: "Abdomenstretch", parent: "Area Stretched" },
+    { label: "Thigh", value: "Thighstretch", parent: "Area Stretched" },
+    { label: "Hip", value: "Hipstretch", parent: "Area Stretched" },
+    {
+      label: "Hamstring",
+      value: "Hamstringstretch",
+      parent: "Area Stretched",
+    },
+    { label: "Third", value: "Third", parent: "Series" },
+    { label: "Second", value: "Second", parent: "Series" },
+    { label: "Primary", value: "Primary", parent: "Series" },
+    { label: "Standing", value: "Standing", parent: "Series" },
   ]);
-  const [stretchItems, setStretchItems] = useState([
-    { label: "Back", value: "Back" },
-    { label: "Abdomen", value: "Abdomen" },
-    { label: "Thigh", value: "Thigh" },
-    { label: "Hip", value: "Hip" },
-    { label: "Hamstring", value: "Hamstring" },
-  ]);
-  const [series, setSeries] = useState([
-    { label: "Standing", value: "Standing" },
-    { label: "Primary", value: "Primary" },
-    { label: "Second", value: "Second" },
-    { label: "Third", value: "Third" },
-  ]);
+  //   const [stretchItems, setStretchItems] = useState([
+  //     { label: "Back", value: "Back" },
+  //     { label: "Abdomen", value: "Abdomen" },
+  //     { label: "Thigh", value: "Thigh" },
+  //     { label: "Hip", value: "Hip" },
+  //     { label: "Hamstring", value: "Hamstring" },
+  //   ]);
+  //   const [series, setSeries] = useState([
+  //     { label: "Standing", value: "Standing" },
+  //     { label: "Primary", value: "Primary" },
+  //     { label: "Second", value: "Second" },
+  //     { label: "Third", value: "Third" },
+  //   ]);
 
   function searchData() {}
 
@@ -46,14 +62,14 @@ export default function Input({ onSubmit }) {
           border: "solid 1px black",
         }}
         open={open}
-        value={strongValue}
-        items={strongItems}
+        value={value}
+        items={items}
         setOpen={setOpen}
-        setValue={setStrongValue}
-        setItems={setStrongItems}
-        placeholder="Search by area strengthened"
+        setValue={setValue}
+        setItems={setItems}
+        placeholder="Search by other fields"
       />
-      <DropDownPicker
+      {/* <DropDownPicker
         style={{
           backgroundColor: "white",
           border: "solid 1px black",
@@ -78,7 +94,7 @@ export default function Input({ onSubmit }) {
         setValue={setSeriesValue}
         setItems={setSeries}
         placeholder="Search by series"
-      />
+      /> */}
       <Button onPress={searchData} title="Search" color="#6608B6" />
     </SafeAreaView>
   );

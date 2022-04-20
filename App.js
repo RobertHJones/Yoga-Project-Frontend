7ca8196100b18cda0a150c513f3ed7b1e4e0d2ec
RobertHJones <roberthuwjones@outlook.com>
2022-04-20 11:46:53 +0100

    basic fetch working

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Display from "./Components/Display";
 import Input from "./Components/Input";
-import API_URL from "./config";
+import { useState } from "react";
+// import API_URL from "./config";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+
+  logo: {
+    width: 200,
+    height: 160,
+  },
+});
 
 export default function App() {
   const [fontsLoaded] = Font.useFonts({
     "Inter-SemiBoldItalic":
       "https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12",
   });
+  const [poses, setPoses] = useState([]);
 
-  async function fetchData(data) {}
+  const API_URL = "https://yoga-database.herokuapp.com/poses";
+
+  async function fetchData(data) {
+    console.log(API_URL);
+    if (data.pose !== "") {
+      const response = await fetch(`${API_URL}/english/${data.pose}`);
+      const poseData = await response.json();
+      console.log(poseData.payload[0]);
+      setPoses(poseData.payload[0]);
+    }
+  }
 
   if (!fontsLoaded) {
     return <AppLoading />;
@@ -36,6 +60,14 @@ export default function App() {
         Ashtanga Companion
       </Text>
       <Input onSubmit={fetchData} />
+      <Display />
+      <Text>{poses.english}</Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: poses.image,
+        }}
+      />
     </View>
   );
 }
